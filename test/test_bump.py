033722f9ab0f892b0e1491d1e43ad2a5f442fcea
William Lin <wlsaidhi@gmail.com>
2017-02-03 19:10:40 -0600

    added unittest for posting

--- test/test_bump.py
+++ test/test_bump.py
@@ -39,13 +39,13 @@ class BumpTestCase(unittest.TestCase):
             email=email,
             password=password,
             confirm=confirm
-        ), follow_redirects=True)
+            ), follow_redirects=True)
     
     def login(self, email, password):
         return self.app.post(USERS_LOGIN_URL, data=dict(
             email=email,
             password=password
-        ), follow_redirects=True)
+            ), follow_redirects=True)
 
     def logout(self):
         return self.app.get(USERS_LOGOUT_URL, follow_redirects=True)
@@ -67,6 +67,40 @@ class BumpTestCase(unittest.TestCase):
 
         rv = self.login('admin@g.com', 'default')
         assert b'Welcome admin!' in rv.data
+
+        rv = self.logout()
+        assert b'You were logged out.' in rv.data
+
+    def post(self, title, text):
+        return self.app.post(POSTS_NEW_POST_URL, data=dict(
+            title=title,
+            text=text
+            ), follow_redirects=True)
+
+    # TODO: figure out handling login status after registering
+    def test_post(self):
+        rv = self.register('admin', 'admin@g.com', 'default', 'default')
+        rv = self.register('admin2', 'admin2@g.com', 'default', 'default')
+        rv = self.login('admin@g.com', 'default')
+        
+        rv = self.post("test","test text")
+        assert b'test text' in rv.data
+        assert b'test | By: admin' in rv.data
+        
+
+        rv = self.logout()
+        assert b'You were logged out.' in rv.data
+        
+        # second user posting
+        rv = self.login('admin2@g.com', 'default')
+        
+        rv = self.post("test2","test text2")
+        assert b'test text2' in rv.data
+        assert b'test2 | By: admin' in rv.data
+        
+        # make sure first post is still there
+        assert b'test text' in rv.data
+        assert b'test | By: admin' in rv.data
 
         rv = self.logout()
         assert b'You were logged out.' in rv.data
--- test/url.py
+++ test/url.py
@@ -6,4 +6,5 @@ USERS_LOGOUT_URL = '/logout/'
 USERS_REGISTER_URL = '/register/'
 
 POSTS_ALL_POSTS_URL = '/posts/'
+POSTS_NEW_POST_URL = '/new_post/'
 
