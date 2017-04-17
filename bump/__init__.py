c608a7aef08b19ce66cd7b600e66135fec738b6c
matthewdias <matthewdias@me.com>
2017-04-16 19:40:43 -0500

    temporarily disable auth

--- bump/__init__.py
+++ bump/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_rest_jsonapi import Api
-from flask_oauthlib.provider import OAuth2Provider, OAuth2RequestValidator
+# from flask_oauthlib.provider import OAuth2Provider, OAuth2RequestValidator
 
 import bump.config as config
 
@@ -27,7 +27,7 @@ DB = SQLAlchemy(APP)
 CORS(APP)
 
 api = Api(APP)
-oauth2 = OAuth2Provider(APP)
+# oauth2 = OAuth2Provider(APP)
 
 # class MyValidator(OAuth2RequestValidator):
 #     def authenticate_client_id(client_id, request, *args, **kwargs):
@@ -36,11 +36,11 @@ oauth2 = OAuth2Provider(APP)
 #     def authenticate_client(request, *args, **kwargs):
         # request.client.client_id = 'test'
 
-import bump.auth
+# import bump.auth
 # from bump.auth import load_client, load_token, load_grant
 
 # oauth2._validator = MyValidator(load_client, load_token, load_grant)
-api.oauth_manager(oauth2)
+# api.oauth_manager(oauth2)
 
 from bump.routes import Route
 
