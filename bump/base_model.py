e0455f0c51ddabf5990ad096836b9b2d49873d67
matthewdias <matthewdias@me.com>
2017-04-20 17:11:47 -0500

    Convert to spaces

--- bump/base_model.py
+++ bump/base_model.py
@@ -3,6 +3,6 @@ from sqlalchemy import Column, DateTime
 from datetime import datetime
 
 class Base(Model):
-	created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, 	default=datetime.utcnow,
-    								onupdate=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime,   default=datetime.utcnow,
+                                    onupdate=datetime.utcnow)
