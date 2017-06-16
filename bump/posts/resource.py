333bd17d948fa4797301dfa01a50e9ae94fc9ad2
matthewdias <matthewdias@me.com>
2017-06-15 23:49:54 -0500

    Revert tuples

--- bump/posts/resource.py
+++ bump/posts/resource.py
@@ -7,13 +7,13 @@ class PostList(ResourceList):
     schema = PostSchema
     data_layer = { 'session': db.session,
                    'model': Post }
-    decorators = (oauth2.require_oauth('email'))
+    decorators = (oauth2.require_oauth('email'),)
 
 class PostDetail(ResourceDetail):
     schema = PostSchema
     data_layer = { 'session': db.session,
                    'model': Post }
-    decorators = (oauth2.require_oauth('email'))
+    decorators = (oauth2.require_oauth('email'),)
 
 class PostRelationship(ResourceRelationship):
     schema = PostSchema
