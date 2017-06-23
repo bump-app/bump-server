781aca83a94f94a5b4c13d85f56793fc92998789
matthewdias <matthewdias@me.com>
2017-06-23 12:55:09 -0500

    Add comments relationships to resources

--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -2,6 +2,7 @@ class UserResource < ApplicationResource
   attributes :email, :password, :name_first, :name_last
 
   has_many :posts
+  has_many :comments
   has_many :user_roles
 
   filter :email
